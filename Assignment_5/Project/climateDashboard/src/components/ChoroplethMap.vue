8c390f3e2a16a71e13a27c1d1a93e11690a3cf85
pegahfj <pfj93@yahoo.com>
2024-01-19 14:53:51 +0100

    choropleth

--- Assignment_5/Project/climateDashboard/src/components/ChoroplethMap.vue
+++ Assignment_5/Project/climateDashboard/src/components/ChoroplethMap.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="vis-component" ref="map">
-    <div ref="choropleth"></div>
+    <svg id="main-svg" :width="svgWidth" :height="svgHeight">
+
+    <g ref="choropleth"></g>
     <!-- <div class="placeholder">
       <b>Here comes the choropleth map</b>.
       <p>Selected states by clicking on the bar chart: {{ selectedStates }}</p>
     </div>
     <svg class="main-svg" :width="svgWidth" :height="svgHeight">
     </svg> -->
+  </svg>
   </div>
 </template>
 
 <script>
 import * as d3 from 'd3';
-import worldData from '@/assets/world.geojson'; // Replace with the path to your GeoJSON file
+// import worldData from '@/assets/world.json'; 
 
 export default {
   name: 'ChoroplethMap',
@@ -27,6 +30,7 @@ export default {
         bottom: 20,
         left: 20,
       },
+      
       colorScale: d3.scaleQuantize().range(['#f1eef6', '#d4b9da', '#c994c7', '#df65b0', '#e7298a', '#ce1256', '#91003f']),
     };
   },
@@ -35,31 +39,30 @@ export default {
   },
   methods: {
     drawChoroplethMap() {
-      const svgWidth = 800;
-      const svgHeight = 500;
-
       const svg = d3.select(this.$refs.choroplethMap)
         .append('svg')
-        .attr('width', svgWidth)
-        .attr('height', svgHeight);
+        .attr('width', this.svgWidth)
+        .attr('height', this.svgHeight);
 
       const projection = d3.geoMercator()
         .scale(120)
-        .translate([svgWidth / 2, svgHeight / 2]);
+        .translate([this.svgWidth / 2, this.svgHeight / 2]);
 
       const path = d3.geoPath().projection(projection);
 
       // Load GeoJSON data
-      d3.json(worldData).then((world) => {
+      d3.json( "@/assets/world.json").then((world) => {
         // Assume your GeoJSON file has a property called 'value' for choropleth coloring
-        this.colorScale.domain([0, d3.max(world.features, d => d.properties.value)]);
+        // this.colorScale.domain([0, d3.max(world.features, d => d.properties.value)]);
 
         svg.selectAll('path')
           .data(world.features)
           .enter()
           .append('path')
           .attr('d', path)
-          .attr('fill', d => this.colorScale(d.properties.value));
+          .attr('fill', 'steelblue');
+
+          // .attr('fill', d => this.colorScale(d.properties.value));
       });
     },
   },
